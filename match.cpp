c71e32bc20eafa3673586a93a0ec9af167eacc4d
Eclipse-Sun <s.eclipse.1314@gmail.com>
2017-05-16 21:29:15 +0800

    Update match.cpp

--- match.cpp
+++ match.cpp
@@ -4,45 +4,54 @@
 
 using namespace std;
 
-struct performance {
-  double lines;
-  double score;
-  double tetrads;
-};
+#define depth1 1
+#define depth2 1
 
-#define depth1 4
-#define depth2 0
+int N = 9;
 
-const int N = 10;
-
-performance playGame() {
-  performance p = {0, 0, 0};
+int playGame(const parameter &p1, const parameter &p2) {
   // freopen("match.txt", "w", stdout);
+  int scoreofBot0 = 0;
   srand((unsigned)time(NULL));
   int blockType, turns;
   int dead;
   Decision p1Decision(0, 0, 0, 0), p2Decision(0, 0, 0, 0);
   for (int i = 0; i < N; ++i) {
-    cout << "Game " << i << endl;
+    cout << "Game " << i << "    ";
     blockType = rand() % 7;
     Node currNode(blockType);
     turns = 0;
     while (1) {
       dead = -1;
       ++turns;
-      //currNode.printField();
-      //cin.get();
-      makeMyDecision(currNode, depth1, depth2);
+      // currNode.printField();
+      // cin.get();
+      makeMyDecision(currNode, depth1, depth2, p1);
       p1Decision = currDecision;
-      if (p1Decision.t == -1) {
-        dead = 0;
-      }
       currBotColor = 1;
       enemyColor = 0;
-      makeMyDecision(currNode, depth1, depth2);
+      makeMyDecision(currNode, depth1, depth2, p2);
       p2Decision = currDecision;
-      if (p2Decision.t == -1 && (dead == -1 || currNode.elimTotal_[1] <= currNode.elimTotal_[0]))
+      if (p1Decision.t == -1) {
+        if (p2Decision.t != -1)
+          dead = 0;
+        else {
+          if (currNode.elimTotal_[0] < currNode.elimTotal_[1])
+            dead = 0;
+          else if (currNode.elimTotal_[0] == currNode.elimTotal_[2]) {
+            dead = 388;
+            ++scoreofBot0;
+          } else {
+            dead = 1;
+            ++scoreofBot0;
+            ++scoreofBot0;
+          }
+        }
+      } else if (p2Decision.t == -1) {
         dead = 1;
+        ++scoreofBot0;
+        ++scoreofBot0;
+      }
       if (dead != -1)
         break;
       currBotColor = 0;
@@ -50,25 +59,50 @@ performance playGame() {
       Node newNode(currNode, p1Decision, p2Decision);
       currNode = newNode;
     }
-    double penalty;
-    if (dead == 0)
-      penalty = 0.8;
-    else
-      penalty = 1;
-    p.lines += currNode.lineTotal_[0] * penalty;
-    p.score += currNode.elimTotal_[0] * penalty;
-    p.tetrads += turns * penalty;
-    cout << currNode.lineTotal_[0] * penalty << ' ' << currNode.elimTotal_[0] * penalty << ' ' << turns * penalty<< ' ' << endl;
+    cout << "Dead player: " << dead << endl;
   }
-  p.lines /= N;
-  p.score /= N;
-  p.tetrads /= N;
-  cout << p.lines << ' ' << p.score << ' ' << p.tetrads << ' ' << endl;
-  return p;
+  return scoreofBot0;
 }
 
+const int H = 20;
+int H_0 = H;
+double eta = 0.01;
+
 int main() {
-  playGame();
-  cin.get();
+  freopen("miaowu~.txt", "w", stderr);
+  clock_t b, e;
+  int res;
+  parameter tmpParameter(currParameter);
+  cerr << "currParameter:   " << currParameter << endl;
+  while (H_0--) {
+    cout << "Round " << H - H_0 << " begins!" << endl;
+    parameter delta = gaussian(currParameter);
+    cout << "currParameter:   " << currParameter << endl;
+    //cout << "delta:           " << delta << endl;
+    parameter p1 = normalization(currParameter + delta),
+              p2 = normalization(currParameter - delta);
+    //cout << "p1 =             " << p1 << endl
+         //<< "p2 =             " << p2 << endl;
+    tmpParameter = currParameter;
+    b = clock();
+    res = playGame(p1, p2); // note that currParameter has changed!
+    e = clock();
+    cout << "After time = " << (e - b) / 1000. << " seconds, the Round is over!"
+         << endl;
+    cout << "Result: " << res << ":" << 2 * N - res << endl;
+    currParameter = tmpParameter + ((eta * (res - N)) * delta);
+    //cout << "Abnormal:        " << currParameter << endl;
+    currParameter = normalization(currParameter);
+    cerr << "currParameter:   " << currParameter << endl;
+    cout << endl;
+  }
+  cout << "Do I become stronger? Let's try to play against the original bot!"
+       << endl;
+  N *= 2;
+  res = playGame(currParameter, normalization(defaultParameter));
+  cout << "Result: Current Bot " << res << " vs Original Bot " << N * 2 - res
+       << endl;
+  cout << "Press enter any number to exit." << endl;
+  cin >> res;
   return 0;
 }
